15021dc5b32ce01a6cf352909daab7a75d31db39
Lubna Febin <lubnafebin13@gmail.com>
2025-09-14 21:41:11 +0530

    updated navbar with sections and react icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,32 @@
+import { FaSearch, FaShoppingCart, FaUser } from "react-icons/fa";
 import "./Navbar.css";
+import logo from "./logo.png";
 export const Navbar = () => {
   return (
     <>
       <div className="navbar">
-        <img src="logo.png" alt="logo" className="logo" />
-        <input type="text" placeholder="Search items" />
-        <button>Search</button>
-        <i>
-          <span>Cart</span>
-        </i>
-        <span>Login</span>
+        <div className="logo">
+          <img src={logo} alt="logo" />
+          <span className="logo-text">VeggieCart</span>
+        </div>
+
+        <div className="search-bar">
+          <input type="text" placeholder="Search items..." />
+          <button className="search-btn">
+            <FaSearch />
+          </button>
+        </div>
+
+        <div className="actions">
+          <div className="cart">
+            <FaShoppingCart />
+            <span className="cart-text">Cart</span>
+          </div>
+          <div className="login">
+            <FaUser />
+            <span>Login</span>
+          </div>
+        </div>
       </div>
     </>
   );
