deea1309265aedfa26006a93d98420da372ed5ba
Lubna Febin <lubnafebin13@gmail.com>
2025-09-15 20:47:24 +0530

    added banner component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import { Banner } from "../components/banner/Banner";
 import { CategoryBar } from "../components/category/CategoryBar";
 
 export const Home = () => {
   return (
     <>
       <CategoryBar />
+      <Banner />
     </>
   );
 };
