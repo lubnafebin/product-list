868b0e74524ac8ea567c8b80055b3f296b79fa88
Lubna Febin <lubnafebin13@gmail.com>
2025-09-15 20:38:32 +0530

    added banner image and content

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,14 @@
+import "./Banner.css";
+import banner from "./banner.webp";
+export const Banner = () => {
+  return (
+    <div className="banner">
+      <img src={banner} alt="vegetables" className="banner-img" />
+      <div className="banner-content">
+        <h2>Fresh Vegetables Big Offer</h2>
+        <p>Get up to 50% off on organic and farm-fresh items.</p>
+        <button className="shop-btn">Shop Now</button>
+      </div>
+    </div>
+  );
+};
