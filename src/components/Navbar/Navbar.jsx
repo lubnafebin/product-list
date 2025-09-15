35178b0bc33a8d085ec7418b8d813a24605fc68a
Lubna Febin <lubnafebin13@gmail.com>
2025-09-15 15:39:13 +0530

    removed fragment and unwanted styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,31 +3,29 @@ import "./Navbar.css";
 import logo from "./logo.png";
 export const Navbar = () => {
   return (
-    <>
-      <div className="navbar">
-        <div className="logo">
-          <img src={logo} alt="logo" />
-          <span className="logo-text">VeggieCart</span>
-        </div>
+    <div className="navbar">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+        <span className="logo-text">VeggieCart</span>
+      </div>
 
-        <div className="search-bar">
-          <input type="text" placeholder="Search items..." />
-          <button className="search-btn">
-            <FaSearch />
-          </button>
-        </div>
+      <div className="search-bar">
+        <input type="text" placeholder="Search items..." />
+        <button className="search-btn">
+          <FaSearch />
+        </button>
+      </div>
 
-        <div className="actions">
-          <div className="cart">
-            <FaShoppingCart />
-            <span className="cart-text">Cart</span>
-          </div>
-          <div className="login">
-            <FaUser />
-            <span>Login</span>
-          </div>
+      <div className="actions">
+        <div className="cart">
+          <FaShoppingCart />
+          <span className="cart-text">Cart</span>
+        </div>
+        <div className="login">
+          <FaUser />
+          <span>Login</span>
         </div>
       </div>
-    </>
+    </div>
   );
 };
