2772cf1208229e566cee8fa02706aa1ff5419c6c
Lubna Febin <lubnafebin13@gmail.com>
2025-09-15 19:23:06 +0530

    added google font style

--- src/components/category/CategoryBar.jsx
+++ src/components/category/CategoryBar.jsx
@@ -3,6 +3,7 @@ export const CategoryBar = () => {
   return (
     <div className="category">
       <ul>
+        <li>All Vegetables</li>
         <li>Vegetables</li>
         <li>Fruits</li>
         <li>Organic</li>
