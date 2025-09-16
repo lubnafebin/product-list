2ed61eec1e8d2c4a4ca828d38517e2f0f95b71f0
Lubna Febin <lubnafebin13@gmail.com>
2025-09-16 21:52:50 +0530

    added responsive design to banner component

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -3,11 +3,13 @@ import banner from "./banner.webp";
 export const Banner = () => {
   return (
     <div className="banner">
-      <img src={banner} alt="vegetables" className="banner-img" />
-      <div className="banner-content">
-        <h2>Fresh Vegetables Big Offer</h2>
-        <p>Get up to 50% off on organic and farm-fresh items.</p>
-        <button className="shop-btn">Shop Now</button>
+      <div className="banner-inner">
+        <img src={banner} alt="vegetables" className="banner-img" />
+        <div className="banner-content">
+          <h2>Fresh Vegetables Big Offer</h2>
+          <p>Get up to 50% off on organic and farm-fresh items.</p>
+          <button className="shop-btn">Shop Now</button>
+        </div>
       </div>
     </div>
   );
