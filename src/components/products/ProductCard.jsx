8d2470d1d16ce0d938d313863eb6d8f6dc7f4d4c
Lubna Febin <lubnafebin13@gmail.com>
2025-09-17 15:45:12 +0530

    update product card with category and rating

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,10 +1,26 @@
-import "./Products.css"
+import { FaRegStar, FaStar, FaStarHalfAlt } from "react-icons/fa";
+import "./Products.css";
 export const ProductCard = ({ product }) => {
+  const rating = 4.5;
   return (
     <div className="product-card">
       <img src={product.image} alt="product" className="product-img" />
+      <p className="product-category">{product.category}</p>
       <h3 className="product-name">{product.name}</h3>
-      <p className="product-price">${product.price}</p>
+      <div className="rating">
+        {rating >= 1 ? <FaStar color="gold" /> : <FaRegStar color="gold" />}
+        {rating >= 2 ? <FaStar color="gold" /> : <FaRegStar color="gold" />}
+        {rating >= 3 ? <FaStar color="gold" /> : <FaRegStar color="gold" />}
+        {rating >= 4 ? <FaStar color="gold" /> : <FaRegStar color="gold" />}
+        {rating >= 4.5 ? (
+          <FaStarHalfAlt color="gold" />
+        ) : (
+          <FaRegStar color="gold" />
+        )}
+      </div>
+      <p className="product-price">
+        <span className="old-price">₹{product.price + 20}</span>₹{product.price}
+      </p>
       <button className="add-btn">Add to Cart</button>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ export const Home = () => {
       id: 1,
       name: "Fresh Tomatoes",
       price: 40,
+      category:"Fruits&Berries",
       image:
         "https://images.unsplash.com/photo-1571680322279-a226e6a4cc2a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dG9tYXRvfGVufDB8fDB8fHww",
     },
@@ -15,6 +16,7 @@ export const Home = () => {
       id: 2,
       name: "Organic Spinach",
       price: 30,
+      category:"Leafy Greens",
       image:
         "https://media.istockphoto.com/id/1322565117/photo/fresh-spinach-leaves-on-wooden-cutting-board-on-old-dark-wooden-table-background-top-view.webp?a=1&b=1&s=612x612&w=0&k=20&c=ka9P9nvZ8p0Mju59cLY0TjA9Q2iuALbzYiQOXqDunkU=",
     },
@@ -22,6 +24,7 @@ export const Home = () => {
       id: 3,
       name: "Carrots",
       price: 50,
+      category:"Root Vegetables",
       image:
         "https://images.unsplash.com/photo-1639086495429-d60e72c53c81?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNhcnJvdHN8ZW58MHx8MHx8fDA%3D",
     },
@@ -29,6 +32,7 @@ export const Home = () => {
       id: 4,
       name: "Broccoli",
       price: 80,
+      category:"Vegetables",
       image:
         "https://plus.unsplash.com/premium_photo-1702403157830-9df749dc6c1e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YnJvY2NvbGl8ZW58MHx8MHx8fDA%3D",
     },
@@ -36,6 +40,7 @@ export const Home = () => {
       id: 5,
       name: "Organic Spinach",
       price: 30,
+      category:"Leafy Greens",
       image:
         "https://media.istockphoto.com/id/1322565117/photo/fresh-spinach-leaves-on-wooden-cutting-board-on-old-dark-wooden-table-background-top-view.webp?a=1&b=1&s=612x612&w=0&k=20&c=ka9P9nvZ8p0Mju59cLY0TjA9Q2iuALbzYiQOXqDunkU=",
     },
