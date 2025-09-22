2e306c2a89434ab5ebbcf971c3959cfd06379b16
Lubna Febin <lubnafebin13@gmail.com>
2025-09-22 19:50:56 +0530

    updated banner for navigation to products page and conditional rendering of navbar on seller pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Home } from "./pages/Home";
 import { CartPage } from "./pages/CartPage";
 import { Login } from "./pages/LoginPage";
 
 function App() {
+  const isSellerPath = useLocation().pathname.includes("seller");
   return (
-    <>
-      <Navbar />
+    <div>
+      {isSellerPath ? null : <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/login" element={<Login />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,6 +1,8 @@
+import { useAppContext } from "../../context/AppContext";
 import "./Banner.css";
 import banner from "./banner.webp";
 export const Banner = () => {
+  const { navigate } = useAppContext();
   return (
     <div className="banner">
       <div className="banner-inner">
@@ -8,7 +10,9 @@ export const Banner = () => {
         <div className="banner-content">
           <h2>Fresh Vegetables Big Offer</h2>
           <p>Get up to 50% off on organic and farm-fresh items.</p>
-          <button className="shop-btn">Shop Now</button>
+          <button className="shop-btn" onClick={() => navigate("/products")}>
+            Shop Now
+          </button>
         </div>
       </div>
     </div>
