138ab6a9e015ac615456d72ec014e67674b136f7
Lubna Febin <lubnafebin13@gmail.com>
2025-09-22 23:32:49 +0530

    feat: implement login component and styles, remove old login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Home } from "./pages/Home";
 import { CartPage } from "./pages/CartPage";
-import { Login } from "./pages/LoginPage";
+import { Login } from "./components/login/Login";
 import { Toaster } from "react-hot-toast";
+import { useAppContext } from "./context/AppContext";
 
 function App() {
   const isSellerPath = useLocation().pathname.includes("seller");
+  const { showUserLogin } = useAppContext();
   return (
     <div>
       <Toaster position="top-right" reverseOrder={false} />
       {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <Login /> : null}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<CartPage />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
