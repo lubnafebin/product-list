ea50bc62b17f6edcf6c4abae85c492a1073f8cd4
Lubna Febin <lubnafebin13@gmail.com>
2025-09-28 20:12:41 +0530

    added getcartamount and count functions

--- src/context/AppContextProvider.jsx
+++ src/context/AppContextProvider.jsx
@@ -220,16 +220,37 @@ export const AppContextProvider = ({ children }) => {
   //clear cart
   const clearCart = () => setCartItems([]);
 
+  //get cart item count
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const item in cartItems) {
+      totalCount += cartItems[item];
+    }
+    return totalCount;
+  };
+
+  //get cart total amount
+  const getCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id === items);
+      if (cartItems[items] > 0) {
+        totalAmount += itemInfo.offerPrice * cartItems[items];
+      }
+    }
+    return Math.floor(totalAmount * 100) / 100;
+  };
+
   useEffect(() => {
     fetchUser();
     fetchSeller();
     fetchProducts();
   }, []);
-  
+
   //update database cartItems
   const updateCart = async () => {
     try {
-      const { data } = await axios.post("/api/cart/update",{cartItems});
+      const { data } = await axios.post("/api/cart/update", { cartItems });
       if (!data.success) {
         toast.error(data.message);
       }
@@ -264,6 +285,8 @@ export const AppContextProvider = ({ children }) => {
     products,
     axios,
     fetchProducts,
+    getCartAmount,
+    getCartCount,
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
