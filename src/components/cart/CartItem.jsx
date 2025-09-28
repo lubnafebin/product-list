51cb718741eb43a07bc5c5085c6a17b6a24116e0
Lubna Febin <lubnafebin13@gmail.com>
2025-09-28 22:09:41 +0530

    added api integration for get user addresses

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -1,14 +1,74 @@
+import { useEffect, useState } from "react";
 import "./CartItem.css";
 import { FaTrash } from "react-icons/fa";
+import { useAppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
 
-export const CartItem = ({ item, removeFromCart }) => {
-  // const [showAddress, setShowAddress] = useState(false);
+export const CartItem = ({ item, removeFromCart, updateCartQuantity }) => {
+  // const address = [
+  //   {
+  //     _id: 1,
+  //     firstName: "jon",
+  //     lastName: "doe",
+  //     email: "j@gmail.com",
+  //     street: "abc",
+  //     city: "xyz",
+  //     state: "yz",
+  //     country: "australia",
+  //     zipcode: 1234,
+  //     phone: "9087654321",
+  //   },
+  //   {
+  //     _id: 2,
+  //     firstName: "jon",
+  //     lastName: "doe",
+  //     email: "j@gmail.com",
+  //     street: "abc",
+  //     city: "xyz",
+  //     state: "yz",
+  //     country: "australia",
+  //     zipcode: 1234,
+  //     phone: "9087654321",
+  //   },
+  // ];
+
+  const [showAddress, setShowAddress] = useState(false);
+  const [addresses, setAddresses] = useState([]);
+  const [selectedAddress, setSelectedAddress] = useState(null);
+  const [paymentOption, setPaymentOption] = useState("COD");
+  const { getCartCount, getCartAmount, navigate, cartItems, axios, user } =
+    useAppContext();
+
+  const getUserAddress = async () => {
+    try {
+      const { data } = await axios.get("/api/address/get");
+      if (data.success) {
+        setAddresses(data.addresses);
+        if (data.address.length > 0) {
+          setSelectedAddress(data.addresses[0]);
+        }
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const placeOrder = async () => {};
+
+  useEffect(() => {
+    if (user) {
+      getUserAddress();
+    }
+  }, [user]);
+
   return (
     <div className="cart-container">
       {/* Left Side - Cart Items */}
       <div className="cart-items">
         <h1 className="cart-title">
-          Review Cart <span className="cart-subtitle">3 Items</span>
+          Review Cart <span className="cart-subtitle">{getCartCount()}</span>
         </h1>
 
         <div className="cart-header">
@@ -20,14 +80,19 @@ export const CartItem = ({ item, removeFromCart }) => {
         <div className="cart-row">
           <div className="cart-product">
             <div className="cart-image">
-              <img src={item.image} alt={item.name} />
+              <img src={item.images[0]} alt={item.name} />
             </div>
             <div>
               <p className="product-name">{item.name}</p>
-              <p className="product-info">Size: {item.size || "N/A"}</p>
+              <p className="product-info">Weight: {item.weight || "N/A"}</p>
               <div className="product-qty">
                 <p>Qty:</p>
-                <select>
+                <select
+                  onChange={(e) =>
+                    updateCartQuantity(item._id, Number(e.target.value))
+                  }
+                  value={cartItems[item._Id]}
+                >
                   {Array(5)
                     .fill("")
                     .map((_, i) => (
@@ -41,14 +106,16 @@ export const CartItem = ({ item, removeFromCart }) => {
           </div>
           <p className="cart-price">${item.offerPrice * item.quantity}</p>
           <button
-            onClick={() => removeFromCart(item.id)}
+            onClick={() => removeFromCart(item._id)}
             className="remove-btn"
           >
             <FaTrash />
           </button>
         </div>
 
-        <button className="continue-btn">← Continue Shopping</button>
+        <button onClick={() => navigate("/")} className="continue-btn">
+          ← Continue Shopping
+        </button>
       </div>
 
       {/* Right Side - Order Summary */}
@@ -59,23 +126,38 @@ export const CartItem = ({ item, removeFromCart }) => {
         <div className="summary-section">
           <p className="section-title">Delivery Address</p>
           <div className="address-box">
-            <p>No address found</p>
+            <p>
+              {selectedAddress
+                ? `${selectedAddress.street},${selectedAddress.city},${selectedAddress.country},${selectedAddress.state}`
+                : "No address found"}
+            </p>
             <button
-              // onClick={() => setShowAddress(!showAddress)}
+              onClick={() => setShowAddress(!showAddress)}
               className="link-btn"
             >
               Change
             </button>
-            {/* {showAddress && (
+            {showAddress && (
               <div className="address-dropdown">
-                <p onClick={() => setShowAddress(false)}>New York, USA</p>
-                <p onClick={() => setShowAddress(false)}>+ Add address</p>
+                {addresses.map((address, index) => (
+                  <p
+                    key={index}
+                    onClick={() => {
+                      setSelectedAddress(address);
+                      setShowAddress(false);
+                    }}
+                  >
+                    {address.street},{address.city},{address.state},
+                    {address.country},
+                  </p>
+                ))}
+                <p onClick={() => navigate("add-addresses")}>+ Add address</p>
               </div>
-            )} */}
+            )}
           </div>
 
           <p className="section-title">Payment Method</p>
-          <select>
+          <select onChange={(e) => setPaymentOption(e.target.value)}>
             <option value="COD">Cash On Delivery</option>
             <option value="Online">Online Payment</option>
           </select>
@@ -85,19 +167,22 @@ export const CartItem = ({ item, removeFromCart }) => {
 
         <div className="price-summary">
           <p>
-            <span>Price</span> <span>$20</span>
+            <span>Price</span> <span>${getCartAmount()}</span>
           </p>
           <p>
             <span>Shipping Fee</span> <span className="green">Free</span>
           </p>
           <p>
-            <span>Tax (2%)</span> <span>$20</span>
+            <span>Tax (2%)</span> <span>${(getCartAmount() * 2) / 100}</span>
           </p>
           <p className="total">
-            <span>Total Amount:</span> <span>$20</span>
+            <span>Total Amount:</span>{" "}
+            <span>${getCartAmount() + (getCartAmount() * 2) / 100}</span>
           </p>
         </div>
-        <button className="place-order-btn">Place Order</button>
+        <button onClick={placeOrder} className="place-order-btn">
+          {paymentOption === "COD" ? "Place Order" : "Proceed to Checkout"}
+        </button>
       </div>
     </div>
   );
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -5,63 +5,62 @@ import { useAppContext } from "../context/AppContext";
 import { FaCheckCircle } from "react-icons/fa";
 
 export const CartPage = () => {
-  const { setCartItems } = useAppContext();
-  // // Update quantity
-  // const updateCartQuantity = (id, newQty) => {
-  //   setCartItems((prev) =>
-  //     prev.map((item) =>
-  //       item.id === id ? { ...item, quantity: newQty } : item
-  //     )
-  //   );
-  // };
-  const cartItems = [
-    {
-      _id: 1,
-      name: "Running Shoes",
-      description: [
-        "Lightweight and comfortable",
-        "Breathable mesh upper",
-        "Ideal for jogging and casual wear",
-      ],
-      offerPrice: 250,
-      price: 200,
-      quantity: 1,
-      size: 42,
-      image:
-        "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/card/productImage.png",
-      category: "Footwear",
-    },
-    // {
-    //   name: "Running Shoes",
-    //   description: [
-    //     "Lightweight and comfortable",
-    //     "Breathable mesh upper",
-    //     "Ideal for jogging and casual wear",
-    //   ],
-    //   offerPrice: 250,
-    //   price: 200,
-    //   quantity: 1,
-    //   size: 42,
-    //   image:
-    //     "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/card/productImage2.png",
-    //   category: "Footwear",
-    // },
-    // {
-    //   name: "Running Shoes",
-    //   description: [
-    //     "Lightweight and comfortable",
-    //     "Breathable mesh upper",
-    //     "Ideal for jogging and casual wear",
-    //   ],
-    //   offerPrice: 250,
-    //   price: 200,
-    //   quantity: 1,
-    //   size: 42,
-    //   image:
-    //     "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/card/productImage3.png",
-    //   category: "Footwear",
-    // },
-  ];
+  const { setCartItems, cartItems } = useAppContext();
+  const updateCartQuantity = (id, newQty) => {
+    setCartItems((prev) =>
+      prev.map((item) =>
+        item.id === id ? { ...item, quantity: newQty } : item
+      )
+    );
+  };
+  // const cartItems = [
+  //   {
+  //     _id: 1,
+  //     name: "Running Shoes",
+  //     description: [
+  //       "Lightweight and comfortable",
+  //       "Breathable mesh upper",
+  //       "Ideal for jogging and casual wear",
+  //     ],
+  //     offerPrice: 250,
+  //     price: 200,
+  //     quantity: 1,
+  //     size: 42,
+  //     image:
+  //       "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/card/productImage.png",
+  //     category: "Footwear",
+  //   },
+  //   {
+  //     name: "Running Shoes",
+  //     description: [
+  //       "Lightweight and comfortable",
+  //       "Breathable mesh upper",
+  //       "Ideal for jogging and casual wear",
+  //     ],
+  //     offerPrice: 250,
+  //     price: 200,
+  //     quantity: 1,
+  //     size: 42,
+  //     image:
+  //       "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/card/productImage2.png",
+  //     category: "Footwear",
+  //   },
+  //   {
+  //     name: "Running Shoes",
+  //     description: [
+  //       "Lightweight and comfortable",
+  //       "Breathable mesh upper",
+  //       "Ideal for jogging and casual wear",
+  //     ],
+  //     offerPrice: 250,
+  //     price: 200,
+  //     quantity: 1,
+  //     size: 42,
+  //     image:
+  //       "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/card/productImage3.png",
+  //     category: "Footwear",
+  //   },
+  // ];
   // Remove item
   const removeFromCart = (id) => {
     setCartItems((prev) => prev.filter((item) => item.id !== id));
@@ -81,6 +80,7 @@ export const CartPage = () => {
               key={item._id}
               item={item}
               removeFromCart={removeFromCart}
+              updateCartQuantity={updateCartQuantity}
             />
           ))}
         </>
