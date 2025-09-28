bf409cb26cd0b8e30f0e50c9eecfef8c879e46d3
Lubna Febin <lubnafebin13@gmail.com>
2025-09-28 20:12:10 +0530

    added address page for shipping address

---
+++ src/pages/AddAddress.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./AddAddress.css"
+
+const InputField = (()=>(
+    <input type="text" />
+))
+
+export const AddAddress = () => {
+  return (
+    <div className='address-container'>
+      <p className='address-title'>Add Shipping <span>Address</span></p>
+    </div>
+  )
+}
+
