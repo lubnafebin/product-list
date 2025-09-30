50796ec7eb4794b614564885c6647f181e528e68
Lubna Febin <lubnafebin13@gmail.com>
2025-09-30 20:16:52 +0530

    added toast for login success

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -22,6 +22,7 @@ export const Login = () => {
         navigate("/");
         setUser(data.user);
         setShowUserLogin(false);
+        toast.success(data.message)
       } else {
         toast.error(data.message);
       }
