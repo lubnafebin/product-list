c21f3476808e58756834ce3587665c7131bdf4cf
Lubna Febin <lubnafebin13@gmail.com>
2025-10-07 20:22:19 +0530

    updated responsive styles for product list page

--- src/pages/seller/ProductList.jsx
+++ src/pages/seller/ProductList.jsx
@@ -24,42 +24,44 @@ export const ProductList = () => {
       <div className="product-list-wrapper">
         <h2 className="product-list-heading">All Products</h2>
         <div className="product-list-table-wrapper">
-          <table className="product-list-table">
-            <thead>
-              <tr>
-                <th>Product</th>
-                <th>Category</th>
-                <th className="hide-mobile">Selling Price</th>
-                <th>In Stock</th>
-              </tr>
-            </thead>
-            <tbody>
-              {products.map((product) => (
-                <tr key={product._id}>
-                  <td className="product-list-product-cell">
-                    <div className="product-list-image-wrapper">
-                      <img src={product.images[0]} alt="Product" />
-                    </div>
-                    <span className="product-list-name">{product.name}</span>
-                  </td>
-                  <td>{product.category}</td>
-                  <td className="hide-mobile">${product.price}</td>
-                  <td>
-                    <label className="switch">
-                      <input
-                        onChange={() =>
-                          toggleStock(product._id, !product.inStock)
-                        }
-                        checked={product.inStock}
-                        type="checkbox"
-                      />
-                      <span className="slider"></span>
-                    </label>
-                  </td>
+          <div className="table-responsive">
+            <table className="product-list-table">
+              <thead>
+                <tr>
+                  <th>Product</th>
+                  <th>Category</th>
+                  <th className="hide-mobile">Selling Price</th>
+                  <th>In Stock</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {products.map((product) => (
+                  <tr key={product._id}>
+                    <td className="product-list-product-cell">
+                      <div className="product-list-image-wrapper">
+                        <img src={product.images[0]} alt="Product" />
+                      </div>
+                      <span className="product-list-name">{product.name}</span>
+                    </td>
+                    <td>{product.category}</td>
+                    <td className="hide-mobile">${product.price}</td>
+                    <td>
+                      <label className="switch">
+                        <input
+                          onChange={() =>
+                            toggleStock(product._id, !product.inStock)
+                          }
+                          checked={product.inStock}
+                          type="checkbox"
+                        />
+                        <span className="slider"></span>
+                      </label>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
