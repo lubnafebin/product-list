c6fa044b6608055cf74f74b26fee5e8b37e0df4e
Lubna Febin <lubnafebin13@gmail.com>
2025-10-11 10:54:39 +0530

    updated cartitems empty object after placing order

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -54,7 +54,7 @@ export const CartPage = () => {
 
         if (data.success) {
           toast.success(data.message);
-          setCartItems([]);
+          setCartItems({});
           navigate("/my-orders");
         } else {
           toast.error(data.message);
